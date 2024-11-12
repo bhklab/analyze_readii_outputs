05ab015910ec898da37195f8f326c8f9dd97110f
Katy Scott <k.l.scott16@gmail.com>
2024-11-12 17:04:57 -0500

    feat: start script version of data processing for prediction models

---
+++ workflow/scripts/python/run_data_setup_for_prediction_models.py
@@ -0,0 +1,27 @@
+# %%
+import sys; sys.path.insert(0, '..')
+ 
+from scripts.python.processing import *
+
+# %%
+def run_data_setup_for_prediction_models(dataset_name:str):
+    
+    pass
+
+# %%
+# Input arguments
+DATASET_NAME = "RADCURE"
+
+# Set variables
+RAW_DATA_PATH = f"../../../rawdata/"
+PROC_DATA_PATH = f"../../../procdata/"
+RESULTS_DATA_PATH = f"../../../results/"
+
+config_file = f"../../config/{DATASET_NAME}.yaml"
+
+
+
+# %%
+
+
+
