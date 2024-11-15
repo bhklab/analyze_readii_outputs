8a65e2d81d302b96b6011ce2fb5cad3b0e016895
Katy Scott <k.l.scott16@gmail.com>
2024-11-15 17:17:46 -0500

    feat: script to run correlation analysis for all HNC data

---
+++ workflow/scripts/python/HNC_correlation_analysis.py
@@ -0,0 +1,7 @@
+from readii_analysis.run_correlation_analysis import run_correlation_analysis
+
+
+run_correlation_analysis(dataset_name="Head-Neck-Radiomics-HN1",
+                         extraction_method="radiomic",
+                         extracted_feature_dir="features/labelled_features_only")
+
